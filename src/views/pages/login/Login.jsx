e58a820b47cbbe0a11254a13c9f9007691afcf8f
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-25 14:20:55 +0300

    Update Login.jsx

--- src/views/pages/login/Login.jsx
+++ src/views/pages/login/Login.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import classes from "./Login.module.scss";
+import {Container, Form} from "react-bootstrap";
 
 const Login = () => {
     return (
-        <div>
-            <h1 className={classes.title}>LOGIN</h1>
-        </div>
+        <section>
+            <Container>
+                <div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                </div>
+            </Container>
+        </section>
     );
 };
 
