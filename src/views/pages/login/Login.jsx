6de5d97eb14a9b51065acc3c4f041aea5a24d47f
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-25 13:57:30 +0300

    ad

---
+++ src/views/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Login.module.scss";
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className={classes.title}>LOGIN</h1>
+        </div>
+    );
+};
+
+export default Login;
