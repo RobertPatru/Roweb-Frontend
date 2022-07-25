336520d0f52146d0d42832a03d95a64f755c35ef
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-25 14:04:16 +0300

    e

--- src/views/container/Router.jsx
+++ src/views/container/Router.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/login";
+import Register from "../pages/register";
+import Home from "../pages/home";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     );
