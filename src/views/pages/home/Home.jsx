d8e9b75df65469eb97bee64e7fd6d2dbc002af21
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-27 16:28:24 +0300

    Working on home page style

--- src/views/pages/home/Home.jsx
+++ src/views/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 import classes from "./Home.module.scss";
+import global_classes from '../../../resources/css/Reusable.module.scss';
+import register_image from '../../../resources/images/welcome-img_1';
+
 
 const Home = () => {
     return (
+        <section className={`${global_classes.full_screen} ${global_classes.align_center}`}  >
+            <img src={register_image} alt="cartoon" className={`${global_classes.on_large_screens_img} ${global_classes.only_on_large_screens}`} />
+
+            <div className={`${global_classes.left_upper_corner_text} ${global_classes.only_on_large_screens}`}>
+                <h1>Welcome to our App!</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, illo!</p>
+            </div>
+
         <div className={classes.container}>
             <ul className={classes.categories_container}>
                 <li style={{"--i":"5"}}><Link to="/login" className='pe-5'>Login</Link></li>
@@ -13,7 +25,8 @@ const Home = () => {
                 <li style={{"--i":"1"}}><Link to="/change-password" className='pe-5'>Change Password</Link></li>
             </ul>
         </div>
+        </section>
     )
 }
 
-export default Home
+export default Home;
