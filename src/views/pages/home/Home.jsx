6ed992bf9bad275aad45dad558494064f55007e0
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-07-27 16:10:06 +0300

    some style for home page

--- src/views/pages/home/Home.jsx
+++ src/views/pages/home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classes from "./Home.module.scss";
 
 const Home = () => {
     return (
-        <div>
-            <Link to="/login" className='pe-5'>Login</Link>
-            <Link to="/register" className='pe-5'>Register</Link>
-            <Link to="/verify-email" className='pe-5'>Verify Email</Link>
-            <Link to="/forgot-password" className='pe-5'>Forgot Password</Link>
-            <Link to="/change-password" className='pe-5'>Change Password</Link>
+        <div className={classes.container}>
+            <ul className={classes.categories_container}>
+                <li style={{"--i":"5"}}><Link to="/login" className='pe-5'>Login</Link></li>
+                <li style={{"--i":"4"}}><Link to="/register" className='pe-5'>Register</Link></li>
+                <li style={{"--i":"3"}}><Link to="/verify-email" className='pe-5'>Verify Email</Link></li>
+                <li style={{"--i":"2"}}><Link to="/forgot-password" className='pe-5'>Forgot Password</Link></li>
+                <li style={{"--i":"1"}}><Link to="/change-password" className='pe-5'>Change Password</Link></li>
+            </ul>
         </div>
     )
 }
