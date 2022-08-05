03f7a61234605afae439fa62acfc8953cc2d136e
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-08-05 20:49:11 +0300

    sidebar installation

--- src/misc/Sidebar/Sidebar.jsx
+++ src/misc/Sidebar/Sidebar.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import classes from './Sidebar.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./Sidebar.module.scss";
+
+import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
 
 const Sidebar = () => {
-	return (
-		<div className={classes.sidebar}>
-			<div>
-				<Link to='/dashboard/profile'>Profile</Link>
-			</div>
-			<div>
-				<Link to='/dashboard/categories'>Categories</Link>
-			</div>
-			<div>
-				<Link to='/dashboard/products'>Products</Link>
-			</div>
-		</div>
-	);
+    return (
+        <div className={classes.sidebar}>
+            <ProSidebar>
+                <Menu iconShape="square">
+                    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
+                    <SubMenu title="Components" icon={<FaHeart />}>
+                        <MenuItem>Component 1</MenuItem>
+                        <MenuItem>Component 2</MenuItem>
+                    </SubMenu>
+                </Menu>
+            </ProSidebar>
+            ;
+        </div>
+    );
 };
 
 export default Sidebar;
