6b2d8a415f5b6c66b2c1c03a555f99b4f79041b0
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2022-08-05 21:17:43 +0300

    default sidebar

--- src/misc/Sidebar/Sidebar.jsx
+++ src/misc/Sidebar/Sidebar.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import classes from "./Sidebar.module.scss";
+import '../../resources/css/Reusable.module.scss';
 
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import {
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 
 const Sidebar = () => {
     return (
         <div className={classes.sidebar}>
             <ProSidebar>
-                <Menu iconShape="square">
-                    <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-                    <SubMenu title="Components" icon={<FaHeart />}>
-                        <MenuItem>Component 1</MenuItem>
-                        <MenuItem>Component 2</MenuItem>
-                    </SubMenu>
-                </Menu>
+                <SidebarHeader>HEDAER</SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="square">
+                        <MenuItem>
+                            Dashboard
+                            <Link to="/dashboard" />
+                        </MenuItem>
+
+                        <SubMenu title="Components">
+                            <MenuItem>Component 1</MenuItem>
+                            <MenuItem>Component 2</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter>
+                   FOOTER
+                </SidebarFooter>
             </ProSidebar>
-            ;
         </div>
     );
 };
